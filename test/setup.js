f56aca0f14d25096fd657dc40a069e4f77e5b924
Lea Hayes <lea@rotorz.com>
2016-03-08 19:07:27 +0000

    Remove import section header comments.

--- test/setup.js
+++ test/setup.js
@@ -2,11 +2,9 @@
 // Licensed under the MIT license. See LICENSE file in the project root.
 
 
-// Packages
 import given from "mocha-testdata";
 import should from "should";
 
-// Project
 import setup from "../lib/setup";
 
 
